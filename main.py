085538e2b2c45c95b3e74fb0b1e7eb39f705d6c0
Arthur Demanuele <ademan19@gmail.com>
2021-12-03 21:25:53 +0100

    q3p2

--- main.py
+++ main.py
@@ -1,43 +1,47 @@
 def main():
     print("Starting...")
-    index = 0
+
+    oxygenBinary = ""
+    scrubBinary = ""
 
     with open('input.txt') as f:
-        lines = f.readlines()
-        oxygenLines = lines
+        lines = f.readlines()        
+        index = 0
 
-        while len(oxygenLines) > 1:
-            oneCountsAtIndex = 0
+        while len(lines) > 1:
+            oneCounts = oneCountsAtIndex(lines, index)
 
-            for line in oxygenLines:                
-                bit = line[index]
-                if bit == "1":
-                    oneCountsAtIndex += 1
-
-            toRemove = "1" if oneCountsAtIndex >= len(oxygenLines) / 2 else "0"
-            oxygenLines = filter(lambda l: l[index] == toRemove, oxygenLines)
+            toRemove = "0" if oneCounts >= len(lines) / 2 else "1"
+            lines = [x for x in lines if x[index] != toRemove]
             index += 1
 
-    index = 0
+        oxygenBinary = lines[0]
+
 
     with open('input.txt') as f:
+        index = 0
         lines = f.readlines()
-        scrubberLines = lines
 
-        while len(scrubberLines) > 1:
-            oneCountsAtIndex = 0
+        while len(lines) > 1:
+            oneCounts = oneCountsAtIndex(lines, index)
 
-            for line in scrubberLines:                
-                bit = line[index]
-                if bit == "1":
-                    oneCountsAtIndex += 1
-
-            toRemove = "1" if oneCountsAtIndex >= len(scrubberLines) / 2 else "0"
-            scrubberLines = filter(lambda l: l[index] == toRemove, scrubberLines)
+            toRemove = "1" if oneCounts >= len(lines) / 2 else "0"
+            lines = [x for x in lines if x[index] != toRemove]
             index += 1
 
-    print(oxygenLines)
-    print(scrubberLines)
+        scrubBinary = lines[0]
+
+    oxygen = int(oxygenBinary, 2)
+    scrub = int(scrubBinary, 2)
+    print(oxygen * scrub)
+
+def oneCountsAtIndex(lines, index):
+    oneCountsAtIndex = 0
+    for line in lines:                
+        bit = line[index]
+        if bit == "1":
+            oneCountsAtIndex += 1
+    return oneCountsAtIndex
 
 if __name__ == "__main__":
     main()
