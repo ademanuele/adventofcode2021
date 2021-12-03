5e0d712193a2fd0425b359dbd3db9b6621104d08
Arthur Demanuele <ademan19@gmail.com>
2021-12-03 19:24:04 +0100

    q3p2

--- main.py
+++ main.py
@@ -1,31 +1,43 @@
-
 def main():
+    print("Starting...")
+    index = 0
+
     with open('input.txt') as f:
         lines = f.readlines()
+        oxygenLines = lines
 
-        print("Starting...")
-        
-        oneCounts = [0] * (len(lines[0]) - 1)
+        while len(oxygenLines) > 1:
+            oneCountsAtIndex = 0
 
-        for line in lines:
-            for i in range(len(line)):
-                bit = line[i]
+            for line in oxygenLines:                
+                bit = line[index]
                 if bit == "1":
-                    oneCounts[i] += 1
+                    oneCountsAtIndex += 1
 
-        gammaC = map(lambda c: "1" if c > len(lines) / 2 else "0", oneCounts)
-        gamma = "".join(str(x) for x in gammaC)
-        gammaInt = int(gamma, 2)
+            toRemove = "1" if oneCountsAtIndex >= len(oxygenLines) / 2 else "0"
+            oxygenLines = filter(lambda l: l[index] == toRemove, oxygenLines)
+            index += 1
 
-        epsilonC = map(lambda c: "0" if c > len(lines) / 2 else "1", oneCounts)
-        epsilon = "".join(str(x) for x in epsilonC)
-        epsilonInt = int(epsilon, 2)
+    index = 0
 
-        print(oneCounts)
-        print(gammaC)
-        print(gammaInt)
-        print(epsilonInt)
-        print(gammaInt * epsilonInt)
+    with open('input.txt') as f:
+        lines = f.readlines()
+        scrubberLines = lines
+
+        while len(scrubberLines) > 1:
+            oneCountsAtIndex = 0
+
+            for line in scrubberLines:                
+                bit = line[index]
+                if bit == "1":
+                    oneCountsAtIndex += 1
+
+            toRemove = "1" if oneCountsAtIndex >= len(scrubberLines) / 2 else "0"
+            scrubberLines = filter(lambda l: l[index] == toRemove, scrubberLines)
+            index += 1
+
+    print(oxygenLines)
+    print(scrubberLines)
 
 if __name__ == "__main__":
     main()
