f6dea4dc4c8ddf10e86e55a8e3910d7d67a69e68
Arthur Demanuele <ademan19@gmail.com>
2021-12-04 16:17:38 +0100

    q4p1

--- main.py
+++ main.py
@@ -1,47 +1,68 @@
+from os import stat
+
+
 def main():
     print("Starting...")
 
-    oxygenBinary = ""
-    scrubBinary = ""
-
     with open('input.txt') as f:
-        lines = f.readlines()        
-        index = 0
-
-        while len(lines) > 1:
-            oneCounts = oneCountsAtIndex(lines, index)
-
-            toRemove = "0" if oneCounts >= len(lines) / 2 else "1"
-            lines = [x for x in lines if x[index] != toRemove]
-            index += 1
-
-        oxygenBinary = lines[0]
-
-
-    with open('input.txt') as f:
-        index = 0
         lines = f.readlines()
 
-        while len(lines) > 1:
-            oneCounts = oneCountsAtIndex(lines, index)
+        drawNumbers = lines[0].split(",")
+        boardLines = []
+        boards = []
 
-            toRemove = "1" if oneCounts >= len(lines) / 2 else "0"
-            lines = [x for x in lines if x[index] != toRemove]
-            index += 1
+        for i in range(2, len(lines)):
+            if lines[i] == "\n":
+                boards.append(BingoBoard(boardLines))
+                boardLines = []
+                continue
+            
+            boardLines.append(lines[i])
 
-        scrubBinary = lines[0]
+        drawn = []
+        for number in drawNumbers:
+            print("Playing: " + number)
+            drawn.append(int(number))
+            for b in boards:
+                b.play(int(number))
+                if b.hasWon():
+                    print(drawn)
+                    print(b.lines)
+                    print(b.unmarkedSum(drawn))
+                    print(b.unmarkedSum(drawn) * int(number))
+                    print("Done")
+                    exit()
 
-    oxygen = int(oxygenBinary, 2)
-    scrub = int(scrubBinary, 2)
-    print(oxygen * scrub)
+class BingoBoard:
+    def __init__(self, lines):
+        self.lines = list(map(BingoBoard._toArrayRow, lines))
+        self._drawnColumns = [0] * 5
+        self._drawnRows = [0] * 5
 
-def oneCountsAtIndex(lines, index):
-    oneCountsAtIndex = 0
-    for line in lines:                
-        bit = line[index]
-        if bit == "1":
-            oneCountsAtIndex += 1
-    return oneCountsAtIndex
+    @staticmethod
+    def _toArrayRow(rowString) -> list[int]:
+        row = list(map(lambda s: s.strip(), rowString.split(" ")))
+        row = list(filter(lambda x: x != "", row))        
+        return list(map(lambda s: int(s), row))
 
+    def play(self, number):
+        for i in range(5):
+            for j in range(5):                
+                if self.lines[i][j] == number:
+                    self._drawnColumns[j] += 1
+                    self._drawnRows[i] += 1                    
+
+    def hasWon(self) -> bool:
+        return 5 in self._drawnColumns or 5 in self._drawnRows        
+
+    def unmarkedSum(self, drawnNumbers) -> int:
+        boardNumbers = []
+        for row in self.lines:
+            for number in row:
+                boardNumbers.append(number)
+        
+        unmarked = list(filter(lambda n: n not in drawnNumbers, boardNumbers))
+        return sum(unmarked)
+        
 if __name__ == "__main__":
     main()
