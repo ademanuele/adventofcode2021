e0b2fb028346954fd1be978e4f466eb509f81620
Arthur Demanuele <ademan19@gmail.com>
2021-12-05 09:27:47 +0100

    q5p1

--- main.py
+++ main.py
@@ -1,79 +1,92 @@
 from os import stat
-
+from collections import defaultdict
+from math import *
 
 def main():
     print("Starting...")
 
     with open('input.txt') as f:
         lines = f.readlines()
+        point_counts = defaultdict(lambda: 0)
 
-        drawNumbers = lines[0].split(",")
-        boardLines = []
-        boards = []
+        for lineS in lines:            
+            line = Line(lineS)
 
-        for i in range(2, len(lines)):
-            if lines[i] == "\n":
-                boards.append(BingoBoard(boardLines))
-                boardLines = []
+            if line.is_diagonal():
                 continue
+
+            for point in line.to_points():
+                point_counts[str(point)] += 1
+
+        count = 0
+        print_grid(point_counts, 20, 20)
+        for i in point_counts.values():
+            if i > 1:
+                count += 1
+
+        print(count)
+
+def print_grid(points, width, height):
+    for i in range(height):
+        for j in range(width):
+            count = points[f"{j}, {i}"]
+            print("." if count == 0 else count, end=" ")
+        print()
             
-            boardLines.append(lines[i])
+class Point:
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
 
-        drawn = []
-        winning_boards = []
-        for number in drawNumbers:
-            print("Playing: " + number)
-            drawn.append(int(number))
-            for b in boards:
-                if b.hasWon():
-                    continue
+    # def __hash__(self) -> int:
+    #     return self.__repr__().__hash__()
 
-                b.play(int(number))
+    # def __eq__(self, __o: object) -> bool:
+    #     return __o is Point and __o.x == self.x and __o.y == self.y
 
-                if b.hasWon():
-                    winning_boards.append(b)
-                    print("Winner: " + str(b.lines))
+    def __repr__(self):
+        return f"{self.x}, {self.y}"
 
-        last_winning_board = winning_boards[-1]        
-        print("Last winning: " + str(last_winning_board.lines))
-        print(last_winning_board.unmarkedSum())
-        print("Done")
-                    
+class Line:
+    def __init__(self, line) -> None:
+        coordinates = line.split(" -> ")
+        startCoordinates = coordinates[0].split(",")
+        endCoordinates = coordinates[1].split(",")
 
-class BingoBoard:
-    def __init__(self, lines):
-        self.lines = list(map(BingoBoard._toArrayRow, lines))
-        self._drawnColumns = [0] * 5
-        self._drawnRows = [0] * 5
-        self._playedNumbers = []
+        self.x1 = int(startCoordinates[0])
+        self.y1 = int(startCoordinates[1])
+        self.x2 = int(endCoordinates[0])
+        self.y2 = int(endCoordinates[1])
 
-    @staticmethod
-    def _toArrayRow(rowString) -> list[int]:
-        row = list(map(lambda s: s.strip(), rowString.split(" ")))
-        row = list(filter(lambda x: x != "", row))        
-        return list(map(lambda s: int(s), row))
+    def is_diagonal(self) -> bool:
+        return self.x1 != self.x2 and self.y1 != self.y2
 
-    def play(self, number):
-        self._playedNumbers.append(number)
-        for i in range(5):
-            for j in range(5):                
-                if self.lines[i][j] == number:
-                    self._drawnColumns[j] += 1
-                    self._drawnRows[i] += 1                    
+    def is_horizontal(self) -> bool:
+        return self.y1 == self.y2
 
-    def hasWon(self):
-        return 5 in self._drawnColumns or 5 in self._drawnRows        
+    def length(self) -> int:
+        return int(sqrt(pow(self.x2 - self.x1, 2) + pow(self.y2 - self.y1, 2)))
 
-    def unmarkedSum(self):
-        boardNumbers = []
-        for row in self.lines:
-            for number in row:
-                boardNumbers.append(number)
-        
-        unmarked = list(filter(lambda n: n not in self._playedNumbers, boardNumbers))
-        s = sum(unmarked)
-        winning_number = self._playedNumbers[len(self._playedNumbers) - 1]
-        return s * winning_number
-        
+    def to_points(self) -> list[Point]:
+        points = []
+
+        x = self.x1
+        y = self.y1
+
+        while (x != self.x2) or (y != self.y2):
+            points.append(Point(x, y))
+            if self.x2 > x:
+                x += 1
+            elif self.x2 < x:
+                x -= 1
+
+            if self.y2 > y:
+                y += 1
+            elif self.y2 < y:
+                y -= 1
+
+        points.append(Point(self.x2, self.y2))
+        return points
+
 if __name__ == "__main__":
     main()
