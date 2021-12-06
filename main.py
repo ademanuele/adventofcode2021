a4bee14c920bf2110fbeb0594fea1d9f888c35de
Arthur Demanuele <ademan19@gmail.com>
2021-12-06 08:10:25 +0100

    q6p1

--- main.py
+++ main.py
@@ -4,91 +4,25 @@ from math import *
 import timeit
 
 def main():
-    start = timeit.default_timer()    
-
     with open('input.txt') as f:
         lines = f.readlines()
-        point_counts = defaultdict(lambda: 0)
-
-        for lineS in lines:            
-            line = Line(lineS)
-
-            for point in line.to_points():
-                point_counts[str(point)] += 1
-
-        count = 0
         
-        for i in point_counts.values():
-            if i > 1:
-                count += 1
+        fish = list(map(lambda f: int(f), lines[0].split(",")))
 
-        print(count)
+        for i in range(256):
+            new_fish_count = 0
+            for f_index in range(len(fish)):
+                if fish[f_index] == 0:
+                    fish[f_index] = 6
+                    new_fish_count += 1
+                    continue
 
-        stop = timeit.default_timer()
+                fish[f_index] -= 1
 
-        print(f"Time: {stop - start}")
+            for i in [8] * new_fish_count:
+                fish.append(i)
 
-def print_grid(points, width, height):
-    for i in range(height):
-        for j in range(width):
-            count = points[f"{j}, {i}"]
-            print("." if count == 0 else count, end=" ")
-        print()
-            
-class Point:
-    def __init__(self, x, y) -> None:
-        self.x = x
-        self.y = y
-
-    # def __hash__(self) -> int:
-    #     return self.__repr__().__hash__()
-
-    # def __eq__(self, __o: object) -> bool:
-    #     return __o is Point and __o.x == self.x and __o.y == self.y
-
-    def __repr__(self):
-        return f"{self.x}, {self.y}"
-
-class Line:
-    def __init__(self, line) -> None:
-        coordinates = line.split(" -> ")
-        startCoordinates = coordinates[0].split(",")
-        endCoordinates = coordinates[1].split(",")
-
-        self.x1 = int(startCoordinates[0])
-        self.y1 = int(startCoordinates[1])
-        self.x2 = int(endCoordinates[0])
-        self.y2 = int(endCoordinates[1])
-
-    def is_diagonal(self) -> bool:
-        return self.x1 != self.x2 and self.y1 != self.y2
-
-    def is_horizontal(self) -> bool:
-        return self.y1 == self.y2
-
-    def length(self) -> int:
-        return int(sqrt(pow(self.x2 - self.x1, 2) + pow(self.y2 - self.y1, 2)))
-
-    def to_points(self) -> list[Point]:
-        points = []
-
-        x = self.x1
-        y = self.y1
-
-        while (x != self.x2) or (y != self.y2):
-            points.append(Point(x, y))
-            if self.x2 > x:
-                x += 1
-            elif self.x2 < x:
-                x -= 1
-
-            if self.y2 > y:
-                y += 1
-            elif self.y2 < y:
-                y -= 1
-
-        points.append(Point(self.x2, self.y2))
-        return points
+        print(len(fish))
 
 if __name__ == "__main__":
     main()
