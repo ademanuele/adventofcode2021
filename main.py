6ead579b53efbba7d767ddf3e856892450517a49
Arthur Demanuele <ademan19@gmail.com>
2021-12-07 08:33:46 +0100

    q7p2

--- main.py
+++ main.py
@@ -6,14 +6,19 @@ import timeit
 def main():
     with open('input.txt') as f:
         positions = f.readlines()[0].split(",")
-        numbers = [int(position) for position in positions]
 
-        costs = []
+    numbers = [int(position) for position in positions]
 
-        for i in range(min(numbers), max(numbers)):
-            costs.append(sum(list(map(lambda n: abs(n - i), numbers))))
+    costs = []
+
+    for target in range(min(numbers), max(numbers)):
+        cost = 0
+        for crab_position in numbers:
+            cost += sum(range(1, abs(crab_position - target) + 1))
+
+        costs.append(cost)
         
-        print(min(costs))
+    print(min(costs))
 
 if __name__ == "__main__":
     main()
