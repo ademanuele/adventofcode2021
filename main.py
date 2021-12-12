bfc3d3ccbe796755133244286f56af856dbda904
Arthur Demanuele <ademan19@gmail.com>
2021-12-12 22:19:53 +0100

    q12p1

--- main.py
+++ main.py
@@ -1,57 +1,72 @@
 from ctypes import sizeof
-from os import stat
+from os import path, stat
 from collections import defaultdict
 from math import *
 import timeit
+import time
 
 def main():
     with open('input.txt') as f:
         lines = f.readlines()
-    
-    flashes = 0
-    oct = list(map(lambda l: list(map(lambda o: int(o), l.strip())), lines))
 
-    iteration = 0
+    nodes = dict()
 
-    while True:
-        iteration += 1
-        # print(str(oct).replace("],", "\n").replace(", ", "").replace("[", "").replace("]", "").replace(" ", ""), end="\n\n")
+    for line in lines:
+        caves = line.strip().split("-")
+        f = Cave(caves[0]) if caves[0] not in nodes else nodes[caves[0]]
+        t = Cave(caves[1]) if caves[1] not in nodes else nodes[caves[1]]
+        t.neighbours.append(f)
+        f.neighbours.append(t)
 
-        for j in range(len(oct)):
-            for i in range(len(oct[j])):
-                oct[j][i] += 1
+        nodes[f.name] = f
+        nodes[t.name] = t
 
-        flashed = []
-        check_again = True
+    print(nodes)
 
-        while check_again:
-            new_flashes = []
-            for j in range(len(oct)):
-                for i in range(len(oct[j])):
+    paths = [[nodes["start"]]]
+    done = False
 
-                    if oct[j][i] > 9 and (j, i) not in flashed:
-                        new_flashes.append((j, i))
-                        adjacent = [(j - 1, i - 1), (j - 1, i), (j - 1, i + 1), (j, i - 1), (j, i + 1), (j + 1, i - 1), (j + 1, i), (j + 1, i + 1)]
-                        for fa in adjacent:
-                            invalid = fa[0] < 0 or fa[1] < 0 or fa[0] >= len(oct) or fa[1] >= len(oct[fa[0]])
-                            if not invalid:
-                                oct[fa[0]][fa[1]] += 1
+    while not done:
+        new_paths = []
+        for path in paths:
+            new_paths += progress(path)
 
-            flashed += new_flashes
+        if len(new_paths) == len(paths):
+            done = True
 
-            if len(new_flashes) == 0:
-                check_again = False
+        paths = new_paths
 
-        for f in flashed:
-            oct[f[0]][f[1]] = 0
+    print(str(paths).replace("],", "\n").replace("[", "").replace(" ", "").replace("]", ""))
+    print(len(paths))
+
+def progress(path):
+    last_node = path[-1]
+
+    if last_node.name == "end":
+        return [path]
+
+    paths = []
+    for neighbour in last_node.neighbours:
+        if neighbour.name == "start":
+            continue
+
+        if neighbour.is_small() and neighbour in path:
+            continue
         
-        if len(flashed) == 100:
-            print(f"HERE: {iteration}")
-            exit()
+        paths.append(list(path) + [neighbour])
 
-        flashes += len(flashed)
+    return paths
 
-    print(flashes)
+class Cave:
+    def __init__(self, name) -> None:
+        self.name = name
+        self.neighbours = []
+
+    def is_small(self) -> bool:
+        return self.name.islower()
+
+    def __repr__(self) -> str:
+        return self.name
 
 if __name__ == "__main__":
     main()
